867ca4615095839f6c21e667453fdb869c221ecf
menna <twmnh195@gmail.com>
2025-01-23 21:55:00 +0200

    v1

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 )
 
+var args, builtins []string
+
 func printOSln(out string) {
 	fmt.Fprintln(os.Stdout, out)
 }
@@ -22,6 +24,49 @@ func contains(s []string, e string) bool {
 	return false
 }
 
+func input(reader *bufio.Reader) []string {
+	fmt.Fprint(os.Stdout, "\033[1m\033[36m$ ")
+	fmt.Fprint(os.Stdout, "\033[1m\033[33m")
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Fprint(os.Stderr, "ERROR: ", err)
+		return []string{}
+	}
+
+	// reset the formatting
+	fmt.Fprint(os.Stdout, "\033[0m")
+
+	return strings.Split(strings.TrimSpace(input), " ")
+}
+
+func redirParser(ins []string) (ofile string, appnd, out, er bool) {
+	for i, in := range ins {
+		if in == ">" || in == "1>" || in == ">>" || in == "2>" || in == "2>>" {
+			if i+1 < len(ins) {
+				ofile = ins[i+1]
+				args = ins[:i]
+			} else {
+				printOSln("arguments not sufficient")
+				continue
+			}
+			if in == ">>" || in == "2>>" {
+				appnd = true
+			}
+			if in == ">" || in == "1>" || in == ">>" {
+				out = true
+			} else if in == "2>" || in == "2>>" {
+				er = true
+			}
+			break
+		}
+	}
+	if ofile == "" {
+		args = ins
+	}
+	return
+}
+
 func redir(appnd bool, ofile string) (*os.File, error) {
 	var file *os.File
 	var err error
@@ -33,9 +78,9 @@ func redir(appnd bool, ofile string) (*os.File, error) {
 	return file, err
 }
 
-func handleExit(ins []string) {
-	if len(ins) > 1 {
-		if ins[1] == "0" {
+func handleExit() {
+	if len(args) > 1 {
+		if args[1] == "0" {
 			os.Exit(0)
 		} else {
 			os.Exit(1)
@@ -45,26 +90,26 @@ func handleExit(ins []string) {
 	}
 }
 
-func handleEcho(ins []string) {
-	if len(ins) > 1 {
-		printOSln(strings.Join(ins[1:], " "))
+func handleEcho() {
+	if len(args) > 1 {
+		printOSln(strings.Join(args[1:], " "))
 	} else {
 		printOSln("")
 	}
 }
 
-func handleType(ins []string, builtins []string) {
-	if len(ins) > 1 {
-		if contains(builtins, ins[1]) {
-			printOSln(ins[1] + " is a shell builtin")
+func handleType() {
+	if len(args) > 1 {
+		if contains(builtins, args[1]) {
+			printOSln(args[1] + " is a shell builtin")
 		} else {
 			// looking for the input command in the paths of the PATH variable
-			path, err := exec.LookPath(ins[1])
+			path, err := exec.LookPath(args[1])
 			if err != nil {
-				printOSln(ins[1] + ": not found")
+				printOSln(args[1] + ": not found")
 				return
 			}
-			printOSln(ins[1] + " is " + path)
+			printOSln(args[1] + " is " + path)
 		}
 	} else {
 		printOSln("")
@@ -80,133 +125,105 @@ func handlePwd() {
 	printOSln(abs)
 }
 
-func handleCd(ins []string) {
+func handleCd() {
 	var err error
-	if len(ins) == 1 || ins[1] == "~" {
+	if len(args) == 1 || args[1] == "~" {
 		home, _ := os.UserHomeDir()
 		err = os.Chdir(home)
 	} else {
-		if ins[1][0] == '/' {
-			ins[1] = strings.TrimPrefix(ins[1], "/")
+		if args[1][0] == '/' {
+			args[1] = strings.TrimPrefix(args[1], "/")
 		}
-		err = os.Chdir(ins[1])
+		err = os.Chdir(args[1])
 	}
 	if err != nil {
-		printOSln("cd: " + ins[1] + ": No such file or directory")
+		printOSln("cd: " + args[1] + ": No such file or directory")
+	}
+}
+
+func builtinsRedirHandle(ofile string, appnd, er, out bool, handler func()) {
+	if ofile != "" {
+		// redirect output to the file
+		file, err := redir(appnd, ofile)
+		if err != nil {
+			printOSln("Error opening file: " + err.Error())
+			return
+		}
+		defer file.Close()
+		if out {
+			// redirect stdout to the file
+			oldStdout := os.Stdout
+			os.Stdout = file
+			handler()
+			os.Stdout = oldStdout // restore stdout
+		} else if er {
+			// redirect stderr to the file
+			oldStderr := os.Stderr
+			os.Stderr = file
+			handler()
+			os.Stderr = oldStderr // restore stderr
+		}
+	} else {
+		handler()
+	}
+}
+
+func externalCmdsHandler(ofile string, appnd, er, out bool) {
+	// handle external commands
+	command := exec.Command(args[0], args[1:]...)
+	if ofile != "" {
+		// redirect output to the file
+		file, err := redir(appnd, ofile)
+		if err != nil {
+			printOSln("Error opening file: " + err.Error())
+			return
+		}
+		defer file.Close()
+
+		if out {
+			// redirect stdout to the file
+			command.Stdout = file
+		} else if er {
+			// redirect stderr to the file
+			command.Stderr = file
+		}
+	} else {
+		command.Stdout = os.Stdout
+		command.Stderr = os.Stderr
+	}
+
+	err := command.Run()
+	if err != nil && !er {
+		printOSln(args[0] + ": command not found")
 	}
 }
 
 func main() {
-	builtins := []string{"type", "echo", "exit", "pwd", "cd"}
+	builtins = []string{"type", "echo", "exit", "pwd", "cd"}
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		// print the shell prompt to the terminal with formatting
-		fmt.Fprint(os.Stdout, "\033[1m\033[36m$ ")
-		fmt.Fprint(os.Stdout, "\033[1m\033[33m")
-
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprint(os.Stderr, "ERROR: ", err)
+		ins := input(reader)
+		if len(ins) == 0 {
+			printOSln("Error with IO\nProgram exit..")
 			return
 		}
 
-		// reset the formatting
-		fmt.Fprint(os.Stdout, "\033[0m")
-
-		ins := strings.Split(strings.TrimSpace(input), " ")
-
-		var ofile string
-		var args []string
-		var appnd, out, er bool
-		for i, in := range ins {
-			if in == ">" || in == "1>" || in == ">>" || in == "2>" || in == "2>>" {
-				if i+1 < len(ins) {
-					ofile = ins[i+1]
-					args = ins[:i]
-				} else {
-					printOSln("arguments not sufficient")
-					continue
-				}
-				if in == ">>" || in == "2>>" {
-					appnd = true
-				}
-				if in == ">" || in == "1>" || in == ">>" {
-					out = true
-				} else if in == "2>" || in == "2>>" {
-					er = true
-				}
-
-				break
-			}
-		}
-		if ofile == "" {
-			args = ins
-		}
+		ofile, appnd, out, er := redirParser(ins)
 
 		switch args[0] {
 		case "exit":
-			handleExit(args)
+			handleExit()
 		case "echo":
-			if ofile != "" {
-				// redirect output to the file
-				file, err := redir(appnd, ofile)
-				if err != nil {
-					printOSln("Error opening file: " + err.Error())
-					continue
-				}
-				defer file.Close()
-				if out {
-					// redirect stdout to the file
-					oldStdout := os.Stdout
-					os.Stdout = file
-					handleEcho(args)
-					os.Stdout = oldStdout // restore stdout
-				} else if er {
-					// redirect stderr to the file
-					oldStderr := os.Stderr
-					os.Stderr = file
-					handleEcho(args)
-					os.Stderr = oldStderr // restore stderr
-				}
-
-			} else {
-				handleEcho(args)
-			}
+			builtinsRedirHandle(ofile, appnd, er, out, handleEcho)
 		case "type":
-			handleType(args, builtins)
+			builtinsRedirHandle(ofile, appnd, er, out, handleType)
 		case "pwd":
-			handlePwd()
+			builtinsRedirHandle(ofile, appnd, er, out, handlePwd)
 		case "cd":
-			handleCd(args)
+			builtinsRedirHandle(ofile, appnd, er, out, handleCd)
 		default:
-			// handle external commands
-			command := exec.Command(args[0], args[1:]...)
-			if ofile != "" {
-				// redirect output to the file
-				file, err := redir(appnd, ofile)
-				if err != nil {
-					printOSln("Error opening file: " + err.Error())
-					continue
-				}
-				defer file.Close()
-
-				if out {
-					// redirect stdout to the file
-					command.Stdout = file
-				} else if er {
-					// redirect stderr to the file
-					command.Stderr = file
-				}
-			} else {
-				command.Stdout = os.Stdout
-				command.Stderr = os.Stderr
-			}
-
-			err := command.Run()
-			if err != nil {
-				printOSln(args[0] + ": command not found")
-			}
+			externalCmdsHandler(ofile, appnd, er, out)
 		}
 	}
 }
